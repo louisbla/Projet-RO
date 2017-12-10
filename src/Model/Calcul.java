8dea3ac92926e26df132b1a1745e11b2a4e4896c
orfire <edouard.blanchard@gmail.com>
2017-12-10 18:58:15 -0500

    prog algo gen

--- src/Model/Calcul.java
+++ src/Model/Calcul.java
@@ -12,25 +12,30 @@ public class Calcul {
     }
     int tempVal;
     int random;
-    for(int tournoi=0;tournoi<100;tournoi++)
+    // on a choisi ici a selection par tournoi avec un nombre de participant de 20 et une population finale de
+    for(int tournoi=0; tournoi<20;tournoi++)
     {
-        for(int k=0;k<ensembleTache.getnbTaches();k++)
+        for(int selection=0;selection<20;selection++)
         {
-            tempOptimalBeforeChange[k]=tempsOptimal[k];
-        }
-        for(int i=0;i<ensembleTache.getnbTaches();i++)
-        {
-            random= ThreadLocalRandom.current().nextInt(0, ensembleTache.getnbTaches());
-            tempVal=tempsOptimal[random];
-            tempsOptimal[random]=tempsOptimal[i];
-            tempsOptimal[i]=tempVal;
-        }
-        if(ensembleTache.calculerTempTraitement(tempsOptimal)>ensembleTache.calculerTempTraitement(tempOptimalBeforeChange))
-        {
+            for(int parcoursTab=0;parcoursTab<ensembleTache.getnbTaches();parcoursTab++)
+            {
+                tempOptimalBeforeChange[parcoursTab]=tempsOptimal[parcoursTab;
+            }
             for(int i=0;i<ensembleTache.getnbTaches();i++)
-            tempsOptimal[i]=tempOptimalBeforeChange[i];
+            {
+                random= ThreadLocalRandom.current().nextInt(0, ensembleTache.getnbTaches());
+                tempVal=tempsOptimal[random];
+                tempsOptimal[random]=tempsOptimal[i];
+                tempsOptimal[i]=tempVal;
+            }
+            if(ensembleTache.calculerTempTraitement(tempsOptimal)>ensembleTache.calculerTempTraitement(tempOptimalBeforeChange))
+            {
+                for(int i=0;i<ensembleTache.getnbTaches();i++)
+                    tempsOptimal[i]=tempOptimalBeforeChange[i];
+            }
         }
     }
+
 
 
         return tempsOptimal;
